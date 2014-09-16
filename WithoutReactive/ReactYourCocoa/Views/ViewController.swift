8153df63e11edf9ac3f25251a52695eb7315f7ec
Tami Wright <tami.wright@gmail.com>
2014-09-16 16:11:42 -0400

    NSTimer with latency bounds implemented

--- WithoutReactive/ReactYourCocoa/Views/ViewController.swift
+++ WithoutReactive/ReactYourCocoa/Views/ViewController.swift
@@ -10,26 +10,32 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    var loginSuccess: Bool
+    var loginTimerTicks: Int
+    var loginTimer: NSTimer?
+    let LATENCY_BOUND: Int = 30;
+    
+    required init(coder aDecoder: NSCoder) {
+        self.loginSuccess = false;
+        self.loginTimerTicks = 0;
+        super.init(coder: aDecoder);
+    }
+
     @IBOutlet var facebookLoginButton: UIButton!
     @IBOutlet var appLogoutButton: UIButton!
     @IBAction func facebookLoginTouch(sender: AnyObject)
     {
+        self.loginTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("checkLoginPermissionSuccess"), userInfo: nil, repeats: true)
         PFFacebookUtils.logInWithPermissions(EWFacebookPermissions.all(), {
             (user: PFUser!, error: NSError!) -> Void in
             if user == nil {
                 NSLog("Uh oh. The user cancelled the Facebook login.")
             } else if user.isNew {
                 NSLog("User signed up and logged in through Facebook!")
-                self.obtainCompleteFacebookPermissions({
-                    self.checkButtonBindings()
-                    self.performSegueWithIdentifier("showMessages", sender: nil)
-                })
+                self.obtainCompleteFacebookPermissions()
             } else {
                 NSLog("User logged in through Facebook!")
-                self.obtainCompleteFacebookPermissions({
-                    self.checkButtonBindings()
-                    self.performSegueWithIdentifier("showMessages", sender: nil)
-                })
+                self.obtainCompleteFacebookPermissions()
             }
         })
     }
@@ -61,6 +67,26 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func checkLoginPermissionSuccess() {
+        self.loginTimerTicks++;
+        // We were able to login and get necessary perms
+        if (self.loginSuccess && self.loginTimer != nil) {
+            self.loginTimer?.invalidate()
+            self.loginTimer = nil
+            self.loginTimerTicks = 0
+            self.checkButtonBindings()
+            self.performSegueWithIdentifier("showMessages", sender: nil)
+        }
+        // Something went wrong and we've exceeded our latency bounds
+        if (self.loginTimerTicks >= LATENCY_BOUND && self.loginTimer != nil && !self.loginSuccess)
+        {
+            self.loginTimer?.invalidate()
+            self.loginTimer = nil
+            self.loginTimerTicks = 0
+            self.loginSuccess = false;
+        }
+    }
+
     func checkButtonBindings()
     {
         if (PFFacebookUtils.isLinkedWithUser(EWUser.currentUser()) || PFTwitterUtils.isLinkedWithUser(EWUser.currentUser())) {
@@ -74,7 +100,7 @@ class LoginViewController: UIViewController {
         }
     }
     
-    func obtainCompleteFacebookPermissions(completion: (() -> Void)? )
+    func obtainCompleteFacebookPermissions()
     {
         if (PFFacebookUtils.isLinkedWithUser(EWUser.currentUser())) {
             var missingReadPermissions = [String]();
@@ -98,9 +124,7 @@ class LoginViewController: UIViewController {
                 FBSession.activeSession().requestNewReadPermissions(missingReadPermissions, completionHandler: { (session:FBSession!, error:NSError!) -> Void in
                     if (error != nil) {
                         NSLog("New read permissions granted")
-                        if ((completion) != nil){
-                            completion!();
-                        }
+                        self.loginSuccess = true;
                     }
                 })
             }
@@ -110,16 +134,12 @@ class LoginViewController: UIViewController {
                         (succeeded: Bool!, error: NSError!) -> Void in
                         if (succeeded != nil) {
                             NSLog("User logged in through Facebook!")
-                            if ((completion) != nil){
-                                completion!();
-                            }
+                            self.loginSuccess = true;
                         }
                 })
             }
             else {
-                if ((completion) != nil){
-                    completion!();
-                }
+                self.loginSuccess = true;
             }
         }
     }
